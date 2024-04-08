d1add8bd1eac4618028238c111f651471098a397
solnyshkoSasha <158512299+solnyshkoSasha@users.noreply.github.com>
2024-04-08 17:20:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var num = 5;
+var some_new = 'Result: ';
+
+console.log(some_new + num + '!');
+
+const number = 10;
+// number = 5;
+console.log(number);
+
+var isJohnHasCar = false;
+num = 'Word';
+console.log(num);
+
+var num1 = 5;
+var num1 = '5';
